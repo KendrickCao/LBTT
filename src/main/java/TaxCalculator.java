e405323008461d04f6529ee1ff01cc8081a6f579
Yanguo Cao <CaoY35@cardiff.ac.uk>
2023-03-23 15:54:32 +0000

    optimize

--- src/main/java/TaxCalculator.java
+++ src/main/java/TaxCalculator.java
@@ -4,42 +4,33 @@ public class TaxCalculator {
     private final int range3 = 325000;
     private final int range4 = 750000;
 
-    public double getRange1Tax(double purchasePrice) {
-        double rate1 = 0;
-        return rate1 * purchasePrice;
-    }
-    public double getRange2Tax(double purchasePrice) {
-        double rate2 = 0.02;
-        return rate2 * (purchasePrice - range1);
-    }
-    public double getRange3Tax(double purchasePrice) {
-        double rate3 = 0.05;
-        return rate3 * (purchasePrice - range2);
-    }
-    public double getRange4Tax(double purchasePrice) {
-        double rate4 = 0.1;
-        return rate4 * (purchasePrice - range3);
-    }
-    public double getRange5Tax(double purchasePrice) {
-        double rate5 = 0.12;
-        return rate5 * (purchasePrice - range4);
+    public double getRangeTax(double purchasePrice, double rate, int range) {
+            return rate * (purchasePrice-range);
     }
 
     public double calculateTaxByPurchasePrice(double purchasePrice){
+        double wholeRange2Tax = getRangeTax(range2,0.02,range1);
+        double wholeRange3Tax = getRangeTax(range3,0.05,range2);
+        double wholeRange4Tax = getRangeTax(range4,0.1,range3);
+
         if (purchasePrice >= 0 && purchasePrice <= range1){
-            return getRange1Tax(purchasePrice);
+            return getRangeTax(purchasePrice,0,0);
         }
         if (purchasePrice > range1 && purchasePrice <= range2){
-            return getRange1Tax(range1) + getRange2Tax(purchasePrice);
+            return getRangeTax(purchasePrice,0.02,range1);
         }
         if (purchasePrice > range2 && purchasePrice <= range3){
-            return getRange1Tax(range1) + getRange2Tax(range2) + getRange3Tax(purchasePrice);
+            double range3Tax = getRangeTax(purchasePrice,0.05,range2);
+            return wholeRange2Tax + range3Tax;
         }
         if (purchasePrice > range3 && purchasePrice <= range4){
-            return getRange1Tax(range1) + getRange2Tax(range2) + getRange3Tax(range3) + getRange4Tax(purchasePrice);
+            double range4Tax = getRangeTax(purchasePrice,0.1,range3);
+            return wholeRange2Tax + wholeRange3Tax + range4Tax;
         }
         if (purchasePrice > range4){
-            return getRange1Tax(range1) + getRange2Tax(range2) + getRange3Tax(range3) + getRange4Tax(range4) + getRange5Tax(purchasePrice);
+            double range5Tax = getRangeTax(purchasePrice,0.12,range4);
+
+            return wholeRange2Tax + wholeRange3Tax + wholeRange4Tax + range5Tax;
         }
         throw new IllegalArgumentException("Input not valid!");
     }
